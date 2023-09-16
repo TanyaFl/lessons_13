f9018738081bb28d7f661071b282a169a21461a7
TanyaFl <tanya.labtex@gmail.com>
2023-09-16 19:48:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿window.onload = () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Lviv&units=metric&APPID=5d066958a60d315387d9492393935c19`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            const weather = data.main;
+            const wind = data.wind;
+
+            const html = `Weather in <b>Lviv</b> - temperature: <b> ${weather.temp} degrees </b>, 
+            pressure: <b>${weather.pressure} units</b>, humidity: <b>${weather.humidity} %</b>
+                wind speed: <b>${wind.speed} m/s</b>.
+            `;
+
+            document.getElementById('weather-container').innerHTML = html;
+        });
+    
+};
